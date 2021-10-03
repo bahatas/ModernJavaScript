85b7bdc61ad2e5d19b23fbdda782c2595a7742ec
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-10-03 11:30:26 +0100

    variable types

--- script.js
+++ script.js
@@ -24,3 +24,45 @@ namee = "Kerem";
 
 console.log(age);
 console.log(namee);
+
+const id = 15; // this line is final varable 
+let id2 = 20;
+
+console.log(id)
+console.log(id)
+
+
+// variable Types 
+
+// Primitive 
+
+//number
+let money = 20;
+//String 
+let firstName = "name";
+//Boolean
+let isActive = true;
+
+//null
+let job = null;
+console.log("type of job " + typeof job)
+    //null is generally part of primitive typoes 
+
+
+//undefined 
+let car;
+console.log("typeof car)" + typeof car)
+
+
+//Reference Types 
+
+//Array 
+let names = ["Baha", "Kerem", "Asli"];
+console.log(names)
+
+//Object
+
+let adress = {
+    "city": "Lancaster",
+    "country": "UK"
+};

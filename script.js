cdd2b36c84878f1994f24abb15d660be88d23945
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-10-03 10:24:23 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("Merhaba");
+
+console.log(123456);
+
+console.log(true);
+console.error("An error occured");
+
+console.warn("A warning ");
+
+
+// first line commnet out
+
+/**
+ * comment lines multi lines 
+ */ //#end region
+
+var age = 21;
+var namee = "Name";
+console.log(age);
+console.log(namee);
+
+age = 11;
+namee = "Kerem";
+
+console.log(age);
+console.log(namee);
